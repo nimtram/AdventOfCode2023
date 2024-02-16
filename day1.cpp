219a9064b666c39a2479c155ea020575f0b42e0b
Martin Indrych <indrychm@gmail.com>
2024-02-16 14:03:56 +0100

    rewrited day1 to functions

--- day1.cpp
+++ day1.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <bits/stdc++.h> 
+
+char getFirstNumber(std::string lineOfFile);
+char getSecondNumber(std::string lineOfFile);
 
 int main()
 {
@@ -9,43 +13,38 @@ std::string lineFromFile;
 std::ifstream MyReadFile("C:/Documents/Prg/AdventOfCode2023/day1_input.txt");
 int sumOfNumbers = 0;
 
-std::string firstDigit;
-std::string secondDigit;
+char firstDigit;
+char secondDigit;
 std::string fullNumber;
-int i;
-while (std::getline (MyReadFile, lineFromFile)) {
-    firstDigit = "x";
-    secondDigit = "x";
-    //std::cout << lineFromFile << std::endl;
-    int lineLength = lineFromFile.length();
-    for (i= 0; i <= lineLength; i++)
-    {
-        if (isdigit(lineFromFile[i])){
-            firstDigit = lineFromFile[i];
-            break;
-        }
-    }
 
-    for (int j = lineLength; j >= 0; j--)
-    {
-        if (isdigit(lineFromFile[j])){
-            secondDigit = lineFromFile[j];
-            break;
-        }
-    }
-    
-    if (firstDigit !="x" && secondDigit!="x" ){
-        fullNumber = firstDigit + secondDigit;
-    }else if (firstDigit!="x")
-    {
-        fullNumber = firstDigit;
-    }
-    
-    
-    std::cout << fullNumber << std::endl;
+while (std::getline (MyReadFile, lineFromFile)) {
+
+    firstDigit = getFirstNumber(lineFromFile);
+    secondDigit = getSecondNumber(lineFromFile);
+
+ std::string fullNumber(1, firstDigit);
+    fullNumber += secondDigit;
     sumOfNumbers = sumOfNumbers + stoi((fullNumber));
 }
 std::cout << sumOfNumbers;
+}
 
+char getFirstNumber(std::string lineOfFile){
+    for (char &letter : lineOfFile) {
+        if (isdigit(letter)){
+           return letter;
+        }
+    }
+    return 'x';
+}
 
+char getSecondNumber(std::string lineOfFile){
+    char letter;
+    for (int iter = lineOfFile.length()-1; iter>=0; iter--){
+    letter = lineOfFile[iter];
+        if (isdigit(letter)){
+           return letter;
+        }
+    }
+    return 'x';
 }
